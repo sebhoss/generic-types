0db55820c6b2d6e36c00155ac347ac88e00b18e3
Sebastian Hoß <mail@shoss.de>
2016-01-17 17:55:16 +0100

    initial commit
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ src/main/java/com/github/sebhoss/utils/types/GenericTypes.java
@@ -0,0 +1,137 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org>
+ */
+package com.github.sebhoss.utils.types;
+
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * Factory to create generic {@link Type} variations, such as <code>Map&lt;String, Object&gt;</code>. Use it as follows:
+ *
+ * <pre>
+ * final Type genericMap = GenericTypes.generic(Map.class, String.class, Object.class);
+ * final Type genericList = GenericTypes.generic(List.class, String.class);
+ * </pre>
+ *
+ * Super- and subtypes such as <code>List&lt;? super Point&gt;</code> or <code>List&lt;? extends Number&gt;</code> can
+ * be created in the following way:
+ *
+ * <pre>
+ * final Type list = GenericTypes.generic(List.class, GenericTypes.supertype(Point.class));
+ * final Type list = GenericTypes.generic(List.class, GenericTypes.subtype(Number.class));
+ * </pre>
+ *
+ * Use static imports to shorten the above calls to:
+ *
+ * <pre>
+ * final Type list = generic(List.class, supertype(Point.class));
+ * final Type list = generic(List.class, subtype(Number.class));
+ * </pre>
+ *
+ * and then go crazy with this:
+ *
+ * <pre>
+ * final Type type = generic(List.class, generic(Map.class, subtype(Number.class), supertype(String.class)));
+ * // represents List&lt;Map&lt;? extends Number, ? super String&gt;&gt;
+ * </pre>
+ */
+public final class GenericTypes {
+
+    /**
+     * Factory method to create a generic {@link Type}.
+     *
+     * @param type
+     *            The outer type.
+     * @param types
+     *            The generic type parameters of the outer type.
+     * @return A {@link Type} representing the given types.
+     */
+    public static final Type generic(final Type type, final Type... types) {
+        return genericType(type, types);
+    }
+
+    /**
+     * Factory method to create a generic subtype.
+     *
+     * @param type
+     *            The supertype.
+     * @return A {@link Type} representing any given subtype of the supplied supertype.
+     */
+    public static final Type subtype(final Type type) {
+        return typeExtends(wildcardType(), type);
+    }
+
+    /**
+     * Factory method to create a generic supertype.
+     *
+     * @param type
+     *            The subtype.
+     * @return A {@link Type} representing any given supertype of the supplied subtype.
+     */
+    public static final Type supertype(final Type type) {
+        return typeSuper(wildcardType(), type);
+    }
+
+    private static LambdaEnabledType genericType(final Type type, final Type... types) {
+        return () -> String.format("%1$s<%2$s>", type.getTypeName(), commaDelimited(types)); //$NON-NLS-1$
+    }
+
+    private static String commaDelimited(final Type... types) {
+        return Arrays.stream(types)
+                .map(Type::getTypeName)
+                .collect(Collectors.joining(", ")); //$NON-NLS-1$
+    }
+
+    private static LambdaEnabledType typeExtends(final Type left, final Type right) {
+        return () -> String.format("%1$s extends %2$s", left.getTypeName(), right.getTypeName()); //$NON-NLS-1$
+    }
+
+    private static LambdaEnabledType typeSuper(final Type left, final Type right) {
+        return () -> String.format("%1$s super %2$s", left.getTypeName(), right.getTypeName()); //$NON-NLS-1$
+    }
+
+    private static LambdaEnabledType wildcardType() {
+        return () -> "?"; //$NON-NLS-1$
+    }
+
+    private GenericTypes() {
+        // factory class
+    }
+
+    /**
+     * Wrapper around {@link Type} in order to be able to create them using lambda expressions (see above)
+     */
+    @FunctionalInterface
+    private static interface LambdaEnabledType extends Type {
+
+        @Override
+        String getTypeName();
+
+    }
+
+}
---
+++ src/test/java/com/github/sebhoss/utils/types/GenericTypesTest.java
@@ -0,0 +1,190 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org>
+ */
+package com.github.sebhoss.utils.types;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.github.sebhoss.warnings.CompilerWarnings;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ */
+@SuppressWarnings({ CompilerWarnings.NLS, CompilerWarnings.STATIC_METHOD })
+public class GenericTypesTest {
+
+    /**
+     *
+     */
+    @Test
+    public void shouldCreateGenericList() {
+        // given
+        final Type genericType = List.class;
+        final Type elementType = String.class;
+
+        // when
+        final Type type = GenericTypes.generic(genericType, elementType);
+
+        // then
+        Assert.assertEquals("java.util.List<java.lang.String>", type.getTypeName());
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void shouldCreateGenericMap() {
+        // given
+        final Type genericType = Map.class;
+        final Type keyType = String.class;
+        final Type valueType = String.class;
+
+        // when
+        final Type type = GenericTypes.generic(genericType, keyType, valueType);
+
+        // then
+        Assert.assertEquals("java.util.Map<java.lang.String, java.lang.String>", type.getTypeName());
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void shouldCreateGenericSet() {
+        // given
+        final Type genericType = Set.class;
+        final Type elementType = String.class;
+
+        // when
+        final Type type = GenericTypes.generic(genericType, elementType);
+
+        // then
+        Assert.assertEquals("java.util.Set<java.lang.String>", type.getTypeName());
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void shouldCreateNestedGenericSet() {
+        // given
+        final Type genericType = Set.class;
+        final Type elementType = GenericTypes.generic(genericType, String.class);
+
+        // when
+        final Type type = GenericTypes.generic(genericType, elementType);
+
+        // then
+        Assert.assertEquals("java.util.Set<java.util.Set<java.lang.String>>", type.getTypeName());
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void shouldCreateGenericWildcardSubtypeList() {
+        // given
+        final Type genericType = List.class;
+        final Type elementType = GenericTypes.subtype(String.class);
+
+        // when
+        final Type type = GenericTypes.generic(genericType, elementType);
+
+        // then
+        Assert.assertEquals("java.util.List<? extends java.lang.String>", type.getTypeName());
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void shouldCreateGenericWildcardSupertypeList() {
+        // given
+        final Type genericType = List.class;
+        final Type elementType = GenericTypes.supertype(String.class);
+
+        // when
+        final Type type = GenericTypes.generic(genericType, elementType);
+
+        // then
+        Assert.assertEquals("java.util.List<? super java.lang.String>", type.getTypeName());
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void shouldCreateNestedGenericWildcardSupertypeList() {
+        // given
+        final Type genericType = List.class;
+        final Type elementType = GenericTypes.generic(genericType, GenericTypes.supertype(String.class));
+
+        // when
+        final Type type = GenericTypes.generic(genericType, elementType);
+
+        // then
+        Assert.assertEquals("java.util.List<java.util.List<? super java.lang.String>>", type.getTypeName());
+    }
+
+    /**
+     * Ensures that the constructor of the {@link GenericTypes} class is private.
+     * <p>
+     * The class should never be instantiated. Instead use the static factory methods to construct storage units.
+     *
+     * @throws NoSuchMethodException
+     *             Should not fail in case the StorageUnits class has a constructor..
+     * @throws IllegalAccessException
+     *             Should not fail in case the StorageUnits class has a constructor..
+     * @throws InvocationTargetException
+     *             Should not fail in case the StorageUnits class has a constructor..
+     * @throws InstantiationException
+     *             Should not fail in case the StorageUnits class has a constructor..
+     */
+    @Test
+    public void shouldDeclarePrivateConstructor()
+            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        // Given
+        final Constructor<GenericTypes> constructor = GenericTypes.class.getDeclaredConstructor();
+
+        // When
+        final boolean isPrivate = Modifier.isPrivate(constructor.getModifiers());
+
+        // Then
+        Assert.assertTrue("Constructor is not private", isPrivate);
+        constructor.setAccessible(true);
+        constructor.newInstance();
+    }
+
+}
